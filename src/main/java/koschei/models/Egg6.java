e77c7913ff947643145f9311da3a040d9ceba85a
smjavac <sm.yusupov@mail.ru>
2024-06-11 00:32:28 +0300

    used all methods

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    @Autowired
     private Needle7 needle;
+    @Autowired
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    @Autowired
+
     private Duck5 duck;
+    @Autowired
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
+    }
+
     @Override
     public String toString() {
         return ", в зайце утка " + duck.toString();
